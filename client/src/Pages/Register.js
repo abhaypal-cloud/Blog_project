051bb40ae09e696767f1cfecaadcade4e9c21d67
Prabhat Munjal <prabhatmunjal74@gmail.com>
2025-12-06 16:01:11 +0530

    Update registration API endpoint URL

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -15,7 +15,7 @@ const Register = () => {
     e.preventDefault();
     try {
         const res = await axios.post(
-            "https://https-github-com-prabhatmunjal12-my-blog.onrender.com/api/v1/user/register",
+            "https://blog-project-5xqq.onrender.com/api/v1/user/register",
             input,
             { headers: { "Content-Type": "application/json" }, withCredentials: true }
         );
